0db73b87ff2091d1cf643e90a2c30f96a632a790
Kaizen711 <vishalnayakawdi711@gmail.com>
2024-12-10 18:38:24 +0530

    Station box added

---
+++ learn_1.py
@@ -0,0 +1,115 @@
+# name = "Pranav"
+# print(name)
+# name = 23
+# print(type(name))
+# name = 23.4
+# print(type(name))
+
+# list
+# fruits = ['apple', 'banana']
+
+# random_things = [1, "Vishal", 23.4, [1,2,3,5], {"name":"Pranav"}]
+
+
+# print(random_things[4])
+
+# dictionary keys and values
+# persons = {0:{"name":"Pranav",
+#               "age":"24"},
+#               2:{"name":"Vishal",
+#                  "age":"23"}}
+
+# print(persons[2]["age"])
+
+# for thing in random_things:
+#     print(thing)
+
+
+# print(type(fruits))
+
+
+
+# persons = {{}}
+
+# Variable types int, string, float, list
+
+
+# Functions
+
+# Defining the function
+# def Addition(a, b):
+#     return a + b
+
+# Calling the function
+#Addition(10,20) = b  # Assignment of variable is always from right to left
+# b = Addition(10,20)  
+# print(b)
+
+# print(Addition("Vishal", "Nayakwadi")) # string concatination
+
+
+
+# def Greet():
+#     print("Hello")
+
+# Greet()
+
+
+
+# loops
+
+# for loop
+
+# for fruits in range(10):
+#     Greet()
+#     print(fruits)
+
+# c++ code
+# for (int i = 0; i<10; i++)
+# {
+#     print(i)
+# }
+
+# for fruit in fruits:
+#     print(fruit)
+
+
+# for person in persons:
+#     print(persons[person])
+
+
+# while loop
+
+# test_variable_for_loop_condition = 20
+# counter = 0
+# while not 1 > 2 :
+    
+#     print("hello")
+#     counter += 1
+#     print(counter)
+#     test_variable_for_loop_condition += 1
+
+# print("after while")
+
+
+
+
+# if, else, else if
+
+# if 10 < 20:
+#     print("True")
+# else:
+#     print("False")
+
+if 10 > 20:
+    print("10 < 20")
+elif 20 > 40:
+    print("20 < 40")
+elif 20 > 40:
+    print("20 < 40")
+elif 20 > 40:
+    print("20 < 40")
+elif 20 < 40:
+    print("20 < 40")
+else:
+    print("all false")
